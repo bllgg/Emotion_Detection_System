ed93ecbc7036ae50e64541835b1a17badffafc6b
bllgg <gunathilakebllg@gmail.com>
2019-02-26 00:49:00 +0000

    Network class created

--- NN_From_Scratch/NN.py
+++ NN_From_Scratch/NN.py
@@ -57,3 +57,62 @@ class Neuron:
             dendron.weight = dendron.weight + dendron.dWeight
             dendron.connectedNeuron.addError(dendron.weight * self.gradient)
         self.error = 0
+
+class Network:
+    def __init__(self, topology):
+        self.layers = []
+        for numNeuron in topology:
+            layer = []
+            for i in range(numNeuron):
+                if (len(self.layers) == 0):
+                    layer.append(Neuron(None))
+                else:
+                    layer.append(Neuron(self.layers[-1]))
+            layer.append(Neuron(None))
+            layer[-1].setOutput(1)
+            self.layers.append(layer)
+
+    def setInput(self, inputs):
+        for i in range(len(inputs)):
+            self.layers[0][i].setOutput(inputs[i])
+
+    def getError(self, target):
+        err = 0
+        for i in range(len(target)):
+            e = (target[i] - self.layers[-1][i].getOutput())
+            err = err + e **2
+        err = err / len(target)
+        err = math.sqrt(err)
+        return err
+
+    def feedForword(self):
+        for layer in self.layers[1:]:
+            for neuron in layer:
+                neuron.feedForword()
+
+    def backPropagate(self, target):
+        for i in range(len(target)):
+            self.layers[-1][i].setError(target[i] - self.layers[-1][i].getOutput())
+        for layer in self.layers[::-1]:
+            for neuron in layer:
+                neuron.backPropagate()
+
+
+    def getResults(self):
+        output = []
+        for neuron in self.layers[-1]:
+            output.append(neuron.getOutput())
+        output.pop() # removing the bias neuron
+        return output
+
+    def getThResults(self):
+        output = []
+        for neuron in self.layers[-1]:
+            o = neuron.getOutput()
+            if (o > 0.5):
+                o = 1
+            else:
+                o = 0
+            output.append(o)
+        output.pop()# removing the bias neuron
+        return output
