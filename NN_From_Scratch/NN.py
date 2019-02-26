b5cd2673d2dbdef06d0ada6925cfdd96109c1d7a
bllgg <gunathilakebllg@gmail.com>
2019-02-26 00:46:45 +0000

    connection class created

---
+++ NN_From_Scratch/NN.py
@@ -0,0 +1,9 @@
+import math
+import numpy as np
+
+class Connection:
+    def __init__(self,connectedNeuron):
+        self.connectedNeuron = connectedNeuron
+        self.weight = np.random.normal()
+        self.dWeight = 0.0
+
